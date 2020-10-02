937219fb4448d7f6454784b3a32b397cafd4f924
bc0599 <betsabecohen123@gmail.com>
2020-10-02 09:01:12 -0400

    login start

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup= new FormGroup({
+    email:new FormControl(null,[Validators.email, Validators.required]),
+    password: new FormControl(null,Validators.required)
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+
+    if(!this.loginForm.valid){
+      console.log('Invalid form')
+
+    }
+
+    console.log(JSON.stringify(this.loginForm.value));
+
   }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  registerForm: FormGroup= new FormGroup({
+
+    email: new FormControl(null,[Validators.email, Validators.required]),
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    cpass: new FormControl(null, Validators.required)
+
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register(){
+    if(!this.registerForm.valid || (this.registerForm.controls.password.value != this.registerForm.controls.cpass.value)){
+
+      console.log('invalid form'); return;
+
+    }
+
+    console.log(JSON.stringify(this.registerForm.value));
   }
 
 }
